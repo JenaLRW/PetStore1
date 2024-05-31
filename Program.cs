a3cdda1b05b9f96f12567895f6198297dd43d5cc
Jena <153410246+JenaLRW@users.noreply.github.com>
2024-05-31 15:28:49 -0400

    Update Program.cs
    did pet store #2

--- Program.cs
+++ Program.cs
@@ -4,55 +4,86 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    { 
-        string userInput = string.Empty; 
+    {
+        string userInput = string.Empty;
 
         Console.WriteLine("Press 1 to add a product");
 
-        Console.WriteLine("type 'exit' to quit"); 
+        Console.WriteLine("Press 8 to view all products");
+
+        Console.WriteLine("type 'exit' to quit");
+
+        var productLogic = new ProductLogic();
 
         userInput = Console.ReadLine();
 
-        while (userInput.ToLower() != "exit")  
-            {        
+        while (userInput.ToLower() != "exit")
+        {
 
-            Console.WriteLine("Enter Product Name");
-
-            userInput = Console.ReadLine(); 
-
-            if (userInput == "dog leash") // using IF statements to expect 2 different answers
+            if (userInput == "1")
             {
-                var dogleash = new DogLeash();
 
-                dogleash.Name = Console.ReadLine();
+                Console.WriteLine("Enter Product Name");
 
-                Console.WriteLine("Enter length"); 
+                userInput = Console.ReadLine();
 
-                dogleash.LengthInches = int.Parse(Console.ReadLine());
 
-                Console.WriteLine("Enter what material product is made of");
 
-                dogleash.Material = Console.ReadLine();
+                if (userInput == "dog leash") 
+                {
+                    var dogleash = new DogLeash();
+
+                    dogleash.Name = userInput;
+
+                    Console.WriteLine("Enter length");
+
+                    dogleash.LengthInches = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter what material product is made of");
+
+                    dogleash.Material = Console.ReadLine();
+
+                    productLogic.AddProducts(dogleash);
+                }
+
+                if (userInput == "cat food")
+                {
+                    var catfood = new CatFood();
+
+                    catfood.Name = userInput;
+
+                    Console.WriteLine("Enter weight in pounds");
+
+                    catfood.WeightPounds = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Is this kitten food? Y or N");
+
+                    catfood.IsKittenFood = Console.ReadLine().ToLower() == "y";
+
+                    productLogic.AddProducts(catfood);
+                }
+
+            }
+            else if (userInput == "8")
+            {
+                var products = productLogic.GetAllProducts();
+
+                foreach (var product in products)
+                {
+                    Console.WriteLine($"{product.Name}");
+                }
+                
             }
 
-            if (userInput == "cat food")
-            {
-                var catfood = new CatFood();
+            Console.WriteLine("Press 1 to add a product");
 
-                catfood.Name = Console.ReadLine();
+            Console.WriteLine("Press 8 to view all products");
 
-                Console.WriteLine("Enter weight in pounds"); 
+            Console.WriteLine("type 'exit' to quit");
 
-                catfood.WeightPounds = double.Parse(Console.ReadLine());
-
-                Console.WriteLine("Is this kitten food? true or false");
-
-                catfood.IsKittenFood = bool.Parse(Console.ReadLine());
-
-            }
+            userInput = Console.ReadLine();
 
         }
-
     }
 }
 
