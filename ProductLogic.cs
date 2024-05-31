880d65b8b3c018c286b999b39caf9f9bd7974c71
Jena <153410246+JenaLRW@users.noreply.github.com>
2024-05-31 15:29:31 -0400

    Add files via upload
    new class for pet store #2

---
+++ ProductLogic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petstore1
+{
+    internal class ProductLogic
+    {
+        private List<Product> _products;
+
+        public void AddProducts(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public ProductLogic()
+        {
+                _products = new List<Product>();
+
+                
+        }
+        public List<Product> GetAllProducts()
+        {
+            return _products;
+        }
+    }
+}
